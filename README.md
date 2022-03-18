9679f898a7548cca4d1535642a2e79858d153dbe
Chi Vong <chivongv@gmail.com>
2022-03-18 08:16:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## System Requirements
+
+- Python 3
+
+## Running
+
+```sh
+python3 calc.py example.txt
+```
+
+## Limitations
+
+- No support for division or other math functions.
+- Reserved commands can not be used as register name e.g. quit
+- Currently only support integer values. Float values will not work.
---
+++ calc.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+
+import sys
+from enum import Enum
+
+mInputs = {}
+mMappedValues = {}
+mVisited = set()
+
+class LoopException(Exception):
+    """Raised when the inputs created a loop"""
+    pass
+
+class Commands(Enum):
+    QUIT = "quit"
+    PRINT = "print"
+
+    def toString():
+        return "print quit"
+
+class Operations(Enum):
+    ADD = "add"
+    SUBTRACT = "subtract"
+    MULTIPLY = "multiply"
+
+    def toString():
+        return "add subtract multiply"
+
+def printUsage():
+    print('Input usage: <register> <operation> <value>' +
+    '\nOperations: ' + Operations.toString() +
+    '\nCommands: ' + Commands.toString() + '\n')
+
+def isNumeric(k):
+    try:
+        a = int(k)
+        return True
+    except ValueError:
+        # if k is not a value, check if k is not in mInputs then add [] to mInputs[k]
+        if(not k in mInputs):
+            mInputs[k] = []
+        return False
+
+def isValidOperator(op):
+    for i in Operations:
+        if(op == i.value):
+            return True
+    return False
+
+def printInvalidCommand(line):
+    print('Command \'' + line + '\' is not valid. Please enter a valid command. Commands: ' + Commands.toString())
+
+def evaluate(reg):
+    if(isNumeric(reg)):
+        return int(reg)
+
+    # returns value if reg already been evaluated
+    if(mMappedValues.get(reg)):
+        return mMappedValues.get(reg)
+
+    if (reg in mVisited):
+        # Here we can either 1. log out error and exit script or 2. throw error and catch it higher up. I choose option 2
+        raise LoopException("Cycle detected. Please check the inputs.")
+    
+    result = 0
+    mVisited.add(reg)
+    # get only the inputs for the current reg
+    regInputs = mInputs[reg]
+
+    for i in regInputs:
+        operator = i[1].lower()
+
+        # skip if mReg is not same as reg
+        if(i[0] != reg):
+            continue
+        
+        if(operator == Operations.ADD.value):
+            result += evaluate(i[2])
+
+        elif(operator == Operations.SUBTRACT.value):
+            result -= evaluate(i[2])
+            
+        elif(operator == Operations.MULTIPLY.value):
+            result *= evaluate(i[2])
+
+    mMappedValues[reg] = result
+    return result
+
+def processLine(line):
+    op = line.lower().split(" ")
+    currentReg = op[0]
+    opLength = len(op)
+
+    if(opLength == 3):
+        operator = op[1].lower()
+
+        if(isNumeric(currentReg)):
+            print("Register '" + currentReg + "' is not valid. Please enter a alphanumeric register name.")
+            return
+
+        if(isValidOperator(operator)):
+            if(currentReg in mInputs):
+                # get inputs for the current reg, append current line and put it in mInputs
+                regInputs = mInputs.get(currentReg)
+                regInputs.append(op)
+                mInputs[currentReg] = regInputs
+            else:
+                mInputs[currentReg] = [op]
+            
+        else:
+            print('Operator \'' + operator + '\' is not supported yet. Please enter one of these operators: ' + Operations.toString())
+
+    elif(opLength == 2):
+        currentCommand = op[0]
+        currentReg = op[1]
+        if(currentCommand.lower() == Commands.PRINT.value):
+            if(currentReg in mInputs):
+                try:
+                    mMappedValues.clear()
+                    mVisited.clear()
+                    print(evaluate(currentReg))
+                except LoopException:
+                    print("Cycle detected. Please check the inputs.")
+            else:
+                print("This register name '" + currentReg + "' does not exists.")
+
+    elif (currentReg == "debug"):
+        print(mInputs)
+        print(mMappedValues)
+
+    else:
+        printInvalidCommand(line)
+
+def main():
+    print('Welcome to simple calc with lazy evaluation')
+    printUsage()
+
+    # Reading files
+    if (len(sys.argv) >= 2):
+        for i in range(1, len(sys.argv)):
+            try:
+                file = open(sys.argv[i], 'r')
+                print("Reading from file \'" + sys.argv[i] + "\'")
+                for line in file:
+                    if(line.lower().strip() == Commands.QUIT.value):
+                        break
+                    processLine(line.strip())
+                file.close()
+                print('')
+                mInputs.clear()
+                mMappedValues.clear()
+            except FileNotFoundError:
+                print('File \'' + sys.argv[i] + '\' can not be found.\n')
+
+    # Read line by line from user
+    else:
+        line = input('Please enter your command: ')
+        while(not line.lower().__contains__(Commands.QUIT.value)):
+            processLine(line)
+            line = input('Please enter your command: ')
+            
+
+if __name__ == "__main__":
+    main()
